cf65a4e671e839dd89cce15764ac9e395c88dcf2
sandra-edathadan <sandraedathadan@gmail.com>
2025-03-02 23:48:15 +0400

    update 1

--- app/crud/read.py
+++ app/crud/read.py
@@ -3,7 +3,8 @@ from typing import List, Optional
 from app.models.item import Item
 
 def get_item(db: Session, item_id: int) -> Optional[Item]:
-    return db.query(Item).filter(Item.id == item_id).first()
+    item = db.query(Item).filter(Item.id == item_id).first()
+    return item
 
 def get_items(db: Session, skip: int = 0, limit: int = 100) -> List[Item]:
     item_list = db.query(Item).offset(skip).limit(limit).all()
