0d6cef7f7c7635f72740a4835a024549e400bf0e
Drashitha03 <f20240420@dubai.bits-pilani.ac.in>
2025-03-03 17:21:25 +0400

    traol.2

--- app/crud/delete.py
+++ app/crud/delete.py
@@ -22,6 +22,8 @@ def delete_item(db: Session, item_id: int) -> bool:
     # 5. Return True to indicate successful deletion
     
     # Delete the code below and implement your solution
+    raise NotImplementedError("The create_item function is not implemented yet")
+
    item = db.query(Item).filter(Item.id == item_id).first()
 if not item:
     return False
